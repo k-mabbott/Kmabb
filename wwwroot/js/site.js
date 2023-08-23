5e924020ed0b777a98d5855d7e11cedd831b1522
k-mabbott <kmabbott9@gmail.com>
2023-08-23 13:36:19 -0700

    Add modal update gallery

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,11 +3,12 @@
 
 // Write your JavaScript code.
 
+// ----------------------------------- Home Page Animations
 
 let sections = document.querySelectorAll('section');
 
 window.onscroll = () => {
-    sections.forEach( sec => {
+    sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop;
         let height = sec.offsetHeight;
@@ -19,4 +20,50 @@ window.onscroll = () => {
             sec.classList.remove('show-animate');
         }
     })
+}
+
+// ----------------------------------- Gallery Scroll to top
+
+// let topBtn = document.getElementById("topBtn");
+
+// window.onscroll = function () { scrollFunction() };
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         topBtn.style.display = "block";
+//     } else {
+//         topBtn.style.display = "none";
+//     }
+// }
+
+// function topFunction() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// }
+
+// ----------------------------------- Gallery Popup Modal
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+let imgs = document.querySelectorAll(".card-img-top");
+var modalImg = document.getElementById("img01");
+
+imgs.forEach( img => {
+    img.onclick = function () {
+        console.log("yup")
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+
+})
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
 }
